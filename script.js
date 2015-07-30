efaa256c58d7316b82e27da0cfb6cc6d2236aafd
unknown <@C-3PO>
2015-07-30 10:40:11 -0500

    Incorporated a search element for my javascript code :)

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var getSearch = function () {
+	var searchInput = $("#searchText").val();
+
+	var matches = $(":contains(" + searchInput  + ")");
+
+	console.log(matches.length);
+	print(matches);
+}
 $(document).ready(function(){
 	
 })
