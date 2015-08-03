98f391862906026e8805c5e70d2b0cf7f6e71bd1
unknown <@C-3PO>
2015-08-03 13:29:34 -0500

    Today I had a double quarter pounder with cheese and ketchup it was aight but they coulda done better and also i did some back end code

--- newJava.js
+++ newJava.js
@@ -1,5 +1,33 @@
-    $('button').click(function() {
-    	var toAdd = $("input[name=message]").val();
-        $('#messages').append("<p>"+toAdd+"</p>");
-    });
-  
+// //$('button').click(function() {
+// 	//var toAdd = $("input[name=message]").val();
+//    // $('#messages').append("<p>"+toAdd+"</p>");
+// });
+
+// function makeAjaxCall (){
+// 	$.ajax({
+// 		url:"http://347e6913.ngrok.com/test"
+// 	})
+// 	.done(function (data) {
+// 		alert(data.message);
+
+// 	})
+// 	.error(function (error) {
+// 		alert(error.message);
+// 	});
+// }
+
+function makeAjaxCall() {
+	var data = $("#dataToSubmit").val();
+	$.ajax({
+		url: "http://347e6913.ngrok.com/test",
+		method: "POST",
+		dataType: "JSON",
+		data: { message: data }
+	})
+	.done(function (data){
+		alert(data.message);
+	})
+	.error(function(error){
+		alert(error.message);
+	});
+}
--- script.js
+++ script.js
@@ -7,4 +7,16 @@ var getSearch = function () {
 
 var clearSearch = function () {
     $(".searchable").css("background-color", "black");
-};
+};//
+function submitEmail () {
+	var email = $("#emailInput").val();
+	$.ajax({
+		type: "POST",
+		url: "http://localhost:3000/join",
+		data: { email: email },
+		success: function (data) {console.log(data); alert("Success")}
+	})
+}
+
+
+
