41c9cbadb6a9f62b3484ea2e5f9aca3a02e42e53
unknown <@C-3PO>
2015-08-05 12:43:36 -0500

    setting up github to talk to azure on the backend

--- script.js
+++ script.js
@@ -17,6 +17,46 @@ function submitEmail () {
 		success: function (data) {console.log(data); alert("Success")}
 	})
 }
+var url = "http://yeahpoetry.azurewebsites.net/join";
+
+function submitEmail () {
+	var email = $("#emailInput").val();
+	if(isValidEmailAddress(email)) {
+		$.ajax({
+		type: "POST",
+		url: url,
+		data: { email: email },
+		success: function (data) {console.log(data); alert("Success");},
+		error: function (error) {
+			var code = error.status;
+			switch (code) {
+				case 500:
+					alert("Something went wrong!");
+					break;
+				case 400:
+					alert("Bad email address!");
+					break;
+				case 409:
+					alert("Email already on the list!");
+					break;
+				default:
+					break;
+				}
+			}
+		});
+
+		$("#emailInput").val("");
+	} else {
+		alert("That's not a valid email address!")
+	}
+}
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    return pattern.test(emailAddress);
+};
+
+
 
 
 
